6b48d41a052f6cf025c3ee1d56cea27f5df958c6
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-12-25 11:56:57 +0700

    feat: add functions to retrieve subnet mask and gateway for network interfaces

--- functions.py
+++ functions.py
@@ -28,6 +28,18 @@ def get_ip_address(interface):
     ipaddress = output.replace("\n", "")
     return ipaddress
 
+def get_subnet_mask(interface):
+    command = [f"ip -o -f inet addr show {interface} | awk '/scope global/ {{print $4}}' | awk -F'/' '{{print $2}}'"]
+    output = bash_command(command=command, universal_newlines=True, shell=True)
+    subnet_mask = output.strip()
+    return subnet_mask
+
+def get_gateway(interface):
+    command = [f"ip route show dev {interface} | awk '/default/ {{print $3}}'"]
+    output = bash_command(command=command, universal_newlines=True, shell=True)
+    gateway = output.replace("\n", "")
+    return gateway
+
 def get_free_ram():
     command= ["free -h | grep Mi"]
     output = bash_command(command, universal_newlines=True, shell=True)
