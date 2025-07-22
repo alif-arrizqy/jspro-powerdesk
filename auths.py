0b4eb27e1fa0dd5e4143b0d2f72ed06c747ebc24
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-07-22 15:03:34 +0700

    fix: update authentication tokens and user passwords for consistency

--- auths.py
+++ auths.py
@@ -3,12 +3,12 @@ from flask_httpauth import HTTPTokenAuth, HTTPBasicAuth
 token_auth = HTTPTokenAuth(scheme='Bearer')
 basic_auth = HTTPBasicAuth()
 tokens = {
-    'd1587d98aa2348b600edc7e7569e3997': 'noc'
+    'RUNDIEpTUFJPIEJBS1RJIDIwMTk=': 'noc'
 }
 
 users = {
     'teknisi': {'password': 'Joulestore2020'},
-    'apt': {'password': 'apt2025'},
+    'apt': {'password': 'powerapt'},
 }
 
 @token_auth.verify_token
@@ -21,6 +21,6 @@ def verify_token(token):
 def verify_password(username, password):
     if username == 'teknisi' and password == 'Joulestore2020':
         return username
-    if username == 'apt' and password == 'apt2025':
+    if username == 'apt' and password == 'powerapt':
         return username
     return None
