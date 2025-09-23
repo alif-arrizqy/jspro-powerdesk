509cddae4250d0865ffbf695ce64c5290e00d7ac
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-09-23 17:02:18 +0700

    refactor: update change_ip script for improved error handling and path configuration

--- commands/change_ip.py
+++ commands/change_ip.py
@@ -2,33 +2,48 @@ from jinja2 import Environment, FileSystemLoader
 import sys
 import os
 
-SRC_PATH = '/var/lib/sundaya/jspro-powerdesk/templates/settingtemplates'
+# Update paths to correct locations
+SRC_PATH = os.path.join(os.path.dirname(__file__), '..', 'templates', 'settingtemplates')
 DEST_PATH_DHCPCD = '/etc/dhcpcd.conf'
 DEST_PATH_SNMP = '/etc/snmp/snmpd.conf'
 
-def dhcpcd_conf(ip,gw,subnet):
-	file_loader = FileSystemLoader(searchpath=SRC_PATH)
-	env = Environment(loader=file_loader)
-	template = env.get_template('dhcpcd.j2')
-	output = template.render(static_ip=ip,gateway=gw,subnet_mask=subnet)
-	log = open(DEST_PATH_DHCPCD,"w")
-	log.write(output)
-	log.close()
-
-# def snmpd_conf(ip):
-# 	file_loader = FileSystemLoader(searchpath=PATH)
-# 	env = Environment(loader=file_loader)
-# 	template = env.get_template('snmpd.j2')
-# 	output = template.render(static_ip=ip)
-# 	log = open(f'{PATH}/snmpd.conf',"w")
-# 	log.write(output)
-# 	log.close()
+def dhcpcd_conf(ip, gw, subnet):
+    """Generate dhcpcd.conf file with static IP configuration"""
+    try:
+        file_loader = FileSystemLoader(searchpath=SRC_PATH)
+        env = Environment(loader=file_loader)
+        template = env.get_template('dhcpcd.j2')
+        output = template.render(static_ip=ip, gateway=gw, subnet_mask=subnet)
+        
+        with open(DEST_PATH_DHCPCD, "w") as log:
+            log.write(output)
+        
+        return True
+    except Exception as e:
+        print(f"Error writing dhcpcd.conf: {e}")
+        return False
 
 if __name__ == '__main__':
-	try:
-		dhcpcd_conf(sys.argv[1],sys.argv[2],sys.argv[3])
-		# snmpd_conf(sys.argv[1])
-		print('Success')
-		os.system('sudo reboot now')
-	except:
-		print('Fail')
+    try:
+        if len(sys.argv) < 4:
+            print('Usage: python3 change_ip.py <ip> <gateway> <subnet>')
+            sys.exit(1)
+        
+        ip = sys.argv[1]
+        gateway = sys.argv[2] 
+        subnet = sys.argv[3]
+        
+        # Generate configuration files
+        dhcp_success = dhcpcd_conf(ip, gateway, subnet)
+        
+        if dhcp_success:
+            print('Success')
+            # Restart networking instead of full reboot for testing
+            # os.system('sudo systemctl restart dhcpcd')
+            # For production, use reboot
+            os.system('sudo reboot now')
+        else:
+            print('Fail')
+            
+    except Exception as e:
+        print(f'Fail: {e}')
--- commands/trigger_change_ip.py
+++ commands/trigger_change_ip.py
@@ -8,4 +8,8 @@ PATH = '/var/lib/sundaya/jspro-powerdesk/commands'
 def change_ip(ip,gw,snmp_ip):
 	p = Popen(["sudo","python3",PATH,ip,gw,snmp_ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE, universal_newlines=True)
 	output, errors = p.communicate()
-	return output
+	# return output
+	if 'Success' in output:
+		return True
+	else:
+		return False
