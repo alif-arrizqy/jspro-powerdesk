a2c49d90262529f84e5f0453b6a24a1246db29fc
alif-arrizqy <alif.arrizqy20@gmail.com>
2025-11-25 11:16:41 +0700

    feat: update install script to include python3-gpiozero and comment out hostapd installation steps for clarity

--- dist/install.sh
+++ dist/install.sh
@@ -8,7 +8,8 @@ sudo apt-get install libssl-dev -y
 sudo apt-get install libffi-dev
 sudo apt-get install python3-setuptools
 sudo apt-get install nginx
-sudo apt-get install hostapd
+sudo apt install python3-gpiozero
+# sudo apt-get install hostapd
 
 echo 'installing jspro-powerdesk requirements'
 sudo pip3 install -r /var/lib/sundaya/jspro-powerdesk/requirements.txt
@@ -28,16 +29,16 @@ sudo systemctl enable webapp.service
 sudo systemctl restart nginx
 # sudo ufw allow 'Nginx Full'
 
-echo 'installing dnsmasq and hostapd'
-sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
-sudo apt-get install -y dnsmasq
-sudo systemctl unmask hostapd
-# sudo systemctl enable hostapd
-sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
-sudo cp /var/lib/sundaya/jspro-powerdesk/dist/dnsmasq.conf /etc/dnsmasq.conf
+# echo 'installing dnsmasq and hostapd'
+# sudo DEBIAN_FRONTEND=noninteractive apt install -y netfilter-persistent iptables-persistent
+# sudo apt-get install -y dnsmasq
+# sudo systemctl unmask hostapd
+# # sudo systemctl enable hostapd
+# sudo mv /etc/dnsmasq.conf /etc/dnsmasq.conf.orig
+# sudo cp /var/lib/sundaya/jspro-powerdesk/dist/dnsmasq.conf /etc/dnsmasq.conf
 
-sudo rfkill unblock wlan
-sudo cp /var/lib/sundaya/jspro-powerdesk/dist/hostapd.conf /etc/hostapd/hostapd.conf
+# sudo rfkill unblock wlan
+# sudo cp /var/lib/sundaya/jspro-powerdesk/dist/hostapd.conf /etc/hostapd/hostapd.conf
 
-echo 'reboot....'
-sudo systemctl reboot
+# echo 'reboot....'
+# sudo systemctl reboot
